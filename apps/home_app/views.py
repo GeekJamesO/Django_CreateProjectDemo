0c11da8b9ae0b32652f1c4672c937d198c6649c6
JamesORourke <geekjamesorourkejr@icloud.com>
2017-08-10 16:51:38 -0700

    added passing to the index page.

--- apps/home_app/views.py
+++ apps/home_app/views.py
@@ -5,5 +5,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    print """I am inside the  the index function
-       of the views...  """
+    # print """I am inside the  the index function
+    #    of the views...  """
+    context = {
+        'num':2,
+        'students': ['Olivier','zarceh','jaims','landece','re']
+    }
+    request.awesome = "Class is awesome"
+    return render(request, "home_app/index.html", context)
